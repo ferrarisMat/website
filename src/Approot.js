892fbb4b36f1bb6fb1d6e59687d17634fe419404
ferrarisMat <mat.ferraris@gmail.com>
2026-02-19 11:17:12 -0500

    feat(Approot): switch from BrowserRouter to HashRouter and add Test route

--- src/Approot.js
+++ src/Approot.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Route, Switch } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Home from './screens/Home';
 import AKFN from './screens/AKFN';
+import Test from './screens/Test'
 
 export default function Approot() {
   return (
-    <BrowserRouter basename="/website">
+    <HashRouter>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/AKFN" exact component={AKFN} />
+        <Route path="/test" exact component={Test} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
